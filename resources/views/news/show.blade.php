b4b9d3e8f14961c24c576dd916f2c573c439f7c9
AnsellC <ansellcruz@gmail.com>
2018-07-13 16:52:33 +0000

    add view news page

---
+++ resources/views/news/show.blade.php
@@ -0,0 +1,51 @@
+@extends('layout')
+@section('title', $news->title)
+@section('content')
+
+    <div class="field">
+        <label class="label">News Source</label>
+        <div class="control">
+            <a href="{{ $news->url }}" target="_blank">{{ $news->url }}</a>
+        </div>
+    </div>
+
+    <div class="field">
+        <label class="label">News Title</label>
+        <div class="control has-icons-left">
+            {{ $news->title }}
+        </div>
+    </div>
+    <div class="field">
+        <label class="label">News Lead</label>
+        <div class="control">
+            {{ $news->lead }}
+        </div>
+    </div>
+    <div class="field">
+        <label class="label">News Content</label>
+        <div class="control">
+            {!! $news->content !!}
+        </div>
+    </div>
+
+    <div class="field">
+        <label class="label">Photo</label>
+        <div class="control">
+            {{ $news->photo }}
+        </div>
+    </div>
+    
+    <div class="field">
+        <label class="label">Status</label>
+            {!! $news->is_finished ? '<i class="fas fa-check"></i>' : '<i class="fas fa-times"></i>' !!} Writer is finished?<br />
+            {!! $news->is_published ? '<i class="fas fa-check"></i>' : '<i class="fas fa-times"></i>' !!} Published to news.ibinex.com? <br />
+            {!! $news->is_seo_published ? '<i class="fas fa-check"></i>' : '<i class="fas fa-times"></i>' !!} Published to social media? <br />
+    </div>
+<div class="columns">
+    <div class="column has-text-centered">
+            <a class="button is-danger" href="/news/{{ $news->id }}/finished">Mark as finished article</a> 
+            <a class="button is-primary" href="/news/{{ $news->id }}/published">Mark as Published on news.ibinex.com</a> 
+            <a class="button is-link" href="/news/{{ $news->id }}/seo">Mark as Published on social media</a>
+    </div>
+</div>
+@endsection
